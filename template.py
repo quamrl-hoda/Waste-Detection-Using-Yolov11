239598cc7ad973b4a472c41545b3affa27eabd94
quamrul hoda <qhoda489@gmail.com>
2026-01-16 14:44:25 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
+
+project_name = "wasteDetection"
+
+list_of_files = [
+  ".github/workflows/.gitkeep",
+  f"src/{project_name}/__init__.py",
+  f"src/{project_name}/components/__init__.py",
+  f"src/{project_name}/components/data_ingestion.py",
+  f"src/{project_name}/components/data_validation.py",
+  f"src/{project_name}/components/model_trainer.py",
+  f"src/{project_name}/utils/__init__.py",
+  f"src/{project_name}/utils/common.py",
+  f"src/{project_name}/constants/__init__.py",
+  f"src/{project_name}/constants/application.py",
+  f"src/{project_name}/entity/__init__.py",
+  f"src/{project_name}/entity/artifact_entity.py",
+  f"src/{project_name}/entity/config_entity.py",
+  f"src/{project_name}/pipeline/__init__.py",
+  f"src/{project_name}/pipeline/training_pipeline.py",
+  f"src/{project_name}/logger/__init__.py",
+  f"src/{project_name}/logger/logger.py",
+  f"src/{project_name}/exception/__init__.py",
+  f"src/{project_name}/exception/custom_exception.py",
+  "config/config.yaml",
+  "params.yaml",
+  "requirements.txt",
+  "setup.py",
+  "research/trials.ipynb",
+  "Dockerfile",
+  "app.py",
+   "main.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir}") 
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Created file: {filepath}")
+    else:
+        logging.info(f"File already exists and is not empty: {filepath}")
